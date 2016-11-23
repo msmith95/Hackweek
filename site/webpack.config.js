9cb73b1586ab648482a407f13ef645b86cfdc8c3
Michael Smith <mdsf3f@mail.missouri.edu>
2016-11-22 19:30:04 -0600

    Initial project push

---
+++ site/webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+
+module.exports = {
+  entry: './js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: './js/'
+  },
+  resolve: {
+	  alias: {
+	    'vue$': 'vue/dist/vue.js'
+	  }
+	},
+	resolveLoader: {
+        root: path.join(__dirname, 'node_modules'),
+    },
+  module: {
+  loaders: [{
+        test: /\.vue$/,
+        loader: 'vue'
+      },{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['latest']
+      }
+    }]
+   }
+};
